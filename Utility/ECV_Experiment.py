f12a08fe2b8465ed87a4a758b8e6aa20f64fe8ba
jepietryga <jacobpietryg2026@u.northwestern.edu>
2022-10-26 16:16:09 -0500

    More classes, improved data tracking

--- Utility/ECV_Experiment.py
+++ Utility/ECV_Experiment.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 class ECV_Experiment():
 
-    def __init__(self,directory_list=[],scan_per_experiment=3,scan_rate_values=[100,150,200]):
+    def __init__(self,directory_list=[],scan_per_experiment=3):
         '''
         Arg:
             directory_list (List[String]): List of paths to folders holding experiment data given in order of experiment
@@ -18,17 +18,19 @@ class ECV_Experiment():
             scan_values    (List[int])   : Values associated with the scan rates (MUST HAVE EQUAL SIZE AS SCAN_PER_EXPERIMENT)
                                             (mV/s)
         '''
-        assert(scan_per_experiment == len(scan_rate_values))
+        #assert(scan_per_experiment == len(scan_rate_values))
 
         self.directory_list = directory_list
         self.scan_per_experiment = scan_per_experiment
-        self.scan_rate_values = scan_rate_values
+        self.experiment_arr = []
+        self.Experiments = []
         self._df = None
 
         # Initiate Paths
-        self._get_group_paths()
+        self._get_experiment_arr()
+        self._create_experiments()
 
-    def _get_group_paths(self):
+    def _get_experiment_arr(self):
         experiment_arr = []
         for directory in self.directory_list:
             # File Read-in
@@ -40,20 +42,23 @@ class ECV_Experiment():
             for ii in np.arange(0,len(file_list),self.scan_per_experiment):
                 try:
                     experiment_ID = file_list[ii]
-                    if not "5k" in experiment_ID: # HARDCODED SOLUTION TO ONE EXPERIMENT
-                        experiment = tuple(
-                                        [file_list[ii+j] for j in np.arange(self.scan_per_experiment)]
-                                    )
-                        experiment_arr.append(experiment)
-                    else:
-                        experiment = tuple(
-                                        [file_list[ii+j] for j in np.arange(self.scan_per_experiment)][::-1]
-                                    )
-                        experiment_arr.append(experiment)
+                    
+                    experiment = tuple(
+                                    [file_list[ii+j] for j in np.arange(self.scan_per_experiment)]
+                                )
+                    experiment_arr.append(experiment)
                 except:
                     print(f'WARNING: Unmatched experiment for {ii}\nLikely scan reset')
         self.experiment_arr = experiment_arr
 
+    def _create_experiments(self):
+        '''
+        Using all grouped scans, create Experiments
+        '''
+        self.Experiments = []
+        for experiment in self.experiment_arr:
+            print(experiment)
+            self.Experiments.append(Experiment(experiment))
     
     def _create_columns(self):
         columns = ['Experiments']
@@ -67,75 +72,6 @@ class ECV_Experiment():
         self.columns = columns
         return columns
 
-    def fit_experiment(self,experiment):
-        '''
-            Return row of a singular experiment, fitting all relevant data
-        '''
-        avg_current_val_arr = []
-        combined_data_arr = []
-
-        # Process Data
-        for run in experiment:
-            try:
-                _,volts_arr,amps_arr = get_cv_values(run)
-                avg_val, nn, mid_volts = get_weighed_max_current(volts_arr,amps_arr)
-
-                avg_current_val_arr.append(avg_val)
-
-                combined_data = list(zip(volts_arr,amps_arr))
-                combined_data_arr.append(combined_data)
-            
-            except:
-                print(f': Error reading {run}, fitting will fail')
-                avg_current_val_arr.append(-1)
-                combined_data_arr.append((-1,-1))
-        
-        # Fit Data
-        m,x = np.polyfit(self.scan_rate_values,avg_current_val_arr,1)
-        fitted_current_arr = [m*scan+x for scan in self.scan_rate_values]
-        r2 = R2(avg_current_val_arr,fitted_current_arr)
-
-        # Store Data
-        new_row = {}
-        new_row["Experiments"] = [experiment]
-
-        ii = 0
-        for scan in self.scan_rate_values:
-            name = f'{scan} mV/s'
-            max_current_string = f'{name} Current (A)'
-            data_string = f'{name} Data ((V,A))'
-            new_row[max_current_string] = [avg_current_val_arr[ii]]
-            new_row[data_string] = [combined_data_arr[ii]]
-            ii += 1
-        new_row["Slope (F)"] = m
-        new_row["Intercept (A)"] = x
-        new_row["R2"]=r2
-
-        return new_row
-
-    def plot_experiment(self,experiment):
-        '''
-           Plot an experiment tuple!
-        '''
-        row = self.fit_experiment(experiment)
-        x = []
-        y = []
-        r2 = row["R2"]
-        m = row["Slope (F)"]
-        b = row["Intercept (A)"]
-        for key, val in row.items():
-            if "Current" in key:
-                x.append(int(key.split(' ')[0]))
-                y.append(val)
-        x = np.array(x)
-        y = np.array(y)
-        fitted_data = x*m+b
-        plt.scatter(x,y,color='red')
-        plt.plot(x,fitted_data,color='black')
-        plt.text(100,max(fitted_data)*.9,f'R$^2$: {r2:.4}\nSlope: {10**3*m:.4} F')
-        plt.xlabel("Scan Rate (mV/s)")
-        plt.ylabel("Current (A)")
-
     @property
     def df(self):
         return self._df
@@ -147,18 +83,134 @@ class ECV_Experiment():
         return self._df
 
     def get_df(self):
-        self._create_columns()
-        df = pd.DataFrame(columns=self.columns)
+        # self._create_columns()
+        # df = pd.DataFrame(columns=self.columns)
+        data = []
 
-        for experiment in self.experiment_arr:
+        for experiment in self.Experiments:
     
-            new_data = self.fit_experiment(experiment)
+            data.append(pd.DataFrame.from_dict(experiment.fitting_row))
 
-            new_row = pd.DataFrame(new_data)
-            df = pd.concat([df,new_row])
+            #new_row = pd.DataFrame(new_data)
+        df = pd.concat(data)
         df.reset_index(inplace=True)
         return df
 
+    def __getitem__(self,i):
+        return self.Experiments[i]
+
+class Experiment():
+
+    def __init__(self,paths=None):
+        '''
+        An experiment covers an individual nanoparticle on grid with all scans associated
+
+        Arg:
+            paths List[Strings] = Paths related to scans of the nanoparticle
+        '''
+        self.paths = paths
+        self.data = {}
+        self.fitting_row = {}
+
+        self._create_data_dict()
+        self._create_fitted_row()
+
+    def _create_data_dict(self):
+        '''
+        For each scan, get experimentally relevant values
+        NOTE: To add more, need to look at the dict provided as 'total_data' and examine it
+        '''
+        x = None
+        y = None
+        for run in self.paths:
+            try:
+                self.data[run] = {}
+                total_data = read_sdc.read_ulv(run)
+                self.data[run]["Time"] = total_data["Data0"] # Seconds
+                self.data[run]["Potential"] = total_data["Data1"] # Volts
+                self.data[run]["Current"] = total_data["Data2"] # Amps
+                self.data[run]["Scan Rate"] = total_data["CVESweepRateVSec"] # V
+                if x == None:
+                    x = total_data["X_Position"]
+                    y = total_data["Y_Position"]
+                self.data[run]["X"] = x
+                self.data[run]["Y"] = x
+            except:
+                print(f'WARNING: {run} failed to read data. Maybe Bad data file?')
+    
+    def _create_fitted_row(self):
+        '''
+            Return row of a singular experiment, fitting all relevant data
+        '''
+        avg_current_val_arr = []
+        combined_data_arr = []
+        scan_rate_values = []
+        # Process Data
+        for run in self.paths:
+            try:
+                run_dict = self.data[run]
+                avg_val, nn, mid_volts = get_weighed_max_current(run_dict["Potential"],run_dict["Current"])
+
+                # For calculation
+                avg_current_val_arr.append(avg_val)
+                scan_rate_values.append(run_dict["Scan Rate"])
+
+                # For later storage and external plot usage
+                combined_data = list(zip(run_dict["Potential"],run_dict["Current"]))
+                combined_data_arr.append(combined_data)
+            
+            except:
+                print(f': Error reading {run}, not included in fitting')
+                #avg_current_val_arr.append(-1)
+                #combined_data_arr.append((-1,-1))
+        
+        # Fit Data
+        m,x = np.polyfit(scan_rate_values,avg_current_val_arr,1)
+        fitted_current_arr = [m*scan+x for scan in scan_rate_values]
+        r2 = R2(avg_current_val_arr,fitted_current_arr)
+
+        # Store Data
+        new_row = {}
+        new_row["Experiments"] = [self.paths]
+
+        ii = 0
+        for scan in scan_rate_values:
+            name = f'{scan} V/s'
+            max_current_string = f'{name} Current (A)'
+            data_string = f'{name} Data ((V,A))'
+            new_row[max_current_string] = [avg_current_val_arr[ii]]
+            new_row[data_string] = [combined_data_arr[ii]]
+            ii += 1
+        new_row["Slope (F)"] = m
+        new_row["Intercept (A)"] = x
+        new_row["R2"]=r2
+
+        self.fitting_row = new_row
+
+    def plot_experiment(self):
+        '''
+           Plot an experiment tuple!
+        '''
+        row = self.fitting_row
+        x = []
+        y = []
+        r2 = row["R2"]
+        m = row["Slope (F)"]
+        b = row["Intercept (A)"]
+        for key, val in row.items():
+            if "Current" in key:
+                x.append(float(key.split(' ')[0]))
+                y.append(val)
+        x = np.array(x)
+        y = np.array(y)
+        fitted_data = x*m+b
+        plt.scatter(x,y,color='red')
+        plt.plot(x,fitted_data,color='black')
+        
+
+        plt.text(min(x),max(fitted_data)*.9,f'R$^2$: {r2:.4}\nSlope: {m:.4} F')
+        plt.xlabel("Scan Rate (V/s)")
+        plt.ylabel("Current (A)")
 
 
 def ucv_number_extractor(file_path):
